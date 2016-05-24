5bd12c2762ac783fcfb63c4320543d4449005e44
Ashley Felton <ashley@ropable.com>
2016-05-24 13:37:07 +0800

    Change urls.py to new-style list.

--- penguins/urls.py
+++ penguins/urls.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
 from observations.sites import site
@@ -12,7 +12,7 @@ router.register(r'count', PenguinCountViewSet)
 router.register(r'observations', PenguinObservationViewSet)
 router.register(r'videos', VideoViewSet)
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^markitup/', include('markitup.urls')),
@@ -20,4 +20,4 @@ urlpatterns = patterns('',
     url(r'^cronjobs/video-import/$', VideoImport.as_view(), name='cronjobs_video_import'),
     url(r'^observations/s3/$', S3View.as_view(), name='s3_view'),
     url(r'', include(site.urls)),
-)
+]
